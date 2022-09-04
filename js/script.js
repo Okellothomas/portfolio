c02fdb610eff048ce75983c23fff074f4595dd02
Okellothomas <thomasolal33@gmail.com>
2022-09-04 18:54:54 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed(".typing", {
-    strings:["An Android Developer","A Web Designer","A Web Developer","An Android Developer","A Full Stack Freelancer", "An Android Developer Frelancer"],
+    strings:["An Android Developer","A Web Designer","A Web Developer","An Android Developer","A Full Stack Freelancer", "An Android Developer Frelancer","Youtuber"],
     typeSpeed:120,
     BackSpeed:80,
     loop:true
